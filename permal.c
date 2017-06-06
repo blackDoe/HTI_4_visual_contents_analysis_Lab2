5c14c78a1ccaa80eec8aaef26fa01901652cc163
blackDoe <blackdoe@mailoo.org>
2017-06-06 20:01:06 +0200

    code avance ..

--- permal.c
+++ permal.c
@@ -31,7 +31,7 @@ void Permal(Matrix2D *mat_in, Matrix2D *mat_out, double dt, int K, int g)
     int           jn, js, iw, ie;
     double        g1e, g1w, g1n, g1s, sigma1;
     double        g2e, g2w, g2n, g2s, sigma2;
-    double        **in, **out;
+    double        **in, **out, **tmp;
 
     in  = mat_in->udata;
     out = mat_out->udata;
@@ -57,10 +57,27 @@ void Permal(Matrix2D *mat_in, Matrix2D *mat_out, double dt, int K, int g)
 
             // Compute gradient components
             // - kernel is not normalized to save 1 multiplication
+            if (g == 1){
+                g1e = g1(in[j][ie]-in[j][i]);
+                g1w = g1(in[j][iw]-in[j][i]);
+                g1n = g1(in[js][i]-in[j][i]);
+                g1s = g1(in[jn][i]-in[j][i]);
+                sigma1 = g1e + g1w + g1n + g1s;
+                // Compute contrast
+                // - kernel normalization is performed here
+                out[j][i] = (int)(in[j][i]+dt*(g1e*in[j][ie] + g1w*in[j][iw] + g1n*in[jn][i] + g1s*in[js][i] - sigma1));
+            }
 
-            // Compute contrast
-            // - kernel normalization is performed here
-            out[j][i] = (int)(0.25*sqrt((double)(gx*gx + gy*gy)));
+            else if(g == 2){
+                g2e = g2(in[j][ie]-in[j][i]);
+                g2w = g2(in[j][iw]-in[j][i]);
+                g2n = g2(in[js][i]-in[j][i]);
+                g2s = g2(in[jn][i]-in[j][i]);
+                sigma2 = g2e + g2w + g2n + g2s;
+                // Compute contrast
+                // - kernel normalization is performed here
+                out[j][i] = (int)(in[j][i]+dt*(g2e*in[j][ie] + g2w*in[j][iw] + g2n*in[jn][i] + g2s*in[js][i] - sigma2));
+            }
         }
     }
 }
@@ -72,7 +89,9 @@ int main(int argc, char *argv[])
     register int   i;
     char           filename_in[256];
     char           filename_out[256];
-    Matrix2D in, out;
+    double         dt, K;
+    int            t, g;
+    Matrix2D in, out, tmp;
 
     sprintf(filename_out, "resultat.pgm");
 
@@ -94,6 +113,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+
+
     /* Lecture et allocation m�moire de l'image d'entr�e */
     read_PGM_file(filename_in, &in);
 
@@ -101,7 +122,13 @@ int main(int argc, char *argv[])
     alloc_Matrix2D(&out, in.xsize, in.ysize, UCHAR_DATA);
 
     /* Traitement */
-    Sobel(&in, &out);    // A remplacer par ***
+    int i;
+    for(i = 0; i < t; i++){
+        Permal(&in, &out, dt, K, g);
+        tmp = out;
+        out = in;
+        in = tmp;
+    }
 
     /* Ecriture de l'image de sortie */
     write_PGM_file(filename_out, &out);
